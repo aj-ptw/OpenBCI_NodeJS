dce72a86f4213b7e6753af695aee5a1479024089
AJ Keller <pushtheworldllc@gmail.com>
2017-04-10 01:04:27 -0400

    Getting the port number through now need to parse but woo

--- examples/wifi/wifiSSDP.js
+++ examples/wifi/wifiSSDP.js
@@ -1,7 +1,26 @@
 const ssdp = require('node-ssdp').Client;
 var http = require('http');
+var net = require('net');
 
 
+////////////////
+// TCP Server //
+////////////////
+var server = net.createServer(function(socket) {
+  socket.on('data', function(data){
+    console.log(data);
+    var textChunk = data.toString('utf8');
+    console.log(textChunk);
+  });
+  socket.on('error', function (err) {
+    console.log(err);
+  })
+}).listen();
+console.log("Server listening on port 5000");
+
+//////////
+// SSDP //
+//////////
 let client = new ssdp({
 //    unicastHost: '192.168.11.63'
 });
@@ -13,7 +32,8 @@ client.on('notify', function () {
 let hitter = null;
 client.on('response', function inResponse(headers, code, rinfo) {
   console.log('Got a response to an m-search:\n%d\n%s\n%s', code, JSON.stringify(headers, null, '  '), JSON.stringify(rinfo, null, '  '));
-  hitThatShit(rinfo.address, '/websocket');
+  // hitThatShit(rinfo.address, '/websocket');
+  post(rinfo.address, '/websocket', {"port": server.address().port});
   // hitter = setInterval(() => {
   //   hitThatShit(rinfo.address);
   // }, 200);
@@ -23,18 +43,6 @@ client.on('response', function inResponse(headers, code, rinfo) {
 client.search('urn:schemas-upnp-org:device:Basic:1');
 
 
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function(socket){
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('data', function(msg) {
-    console.log('message: ' + msg);
-  });
-});
-server.listen(3000);
 
 // And after 10 seconds, you want to stop
 setTimeout(function () {
@@ -43,46 +51,39 @@ setTimeout(function () {
   client.stop()
 }, 5 * 60 * 1000);
 
-const hitThatShit = (host, path) => {
+const post = (host, path, payload) => {
   //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-  var options = {
+  const output = JSON.stringify(payload);
+  const options = {
     host: host,
-    path: path
+    port: 80,
+    path: path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': output.length
+    }
   };
 
-  const callback = (response) => {
-    let str = '';
-    //another chunk of data has been recieved, so append it to `str`
-    response.on('data', function (chunk) {
-      str += chunk;
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
     });
-    //the whole response has been recieved, so we just print it out here
-    response.on('end', function () {
-      console.log(str);
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
 
-      // if (str !== "") {
-      //   try {
-      //     const packetObj = JSON.parse(str);
-      //     const numSamples = packetObj.data.length;
-      //
-      //     console.log(numSamples);
-      //   } catch (err) {
-      //     console.log(err);
-      //   }
-      // } else {
-      //   console.log(str);
-      // }
-    });
-    response.on('error', function(err) {
-      console.log(err);
-    });
-  };
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+  });
 
-  try {
-    http. request(options, callback).end();
-  } catch (err) {
-    console.log(err);
-  }
+// write data to request body
+  req.write(output);
+  req.end();
 };
 
 console.log("Server ready");
